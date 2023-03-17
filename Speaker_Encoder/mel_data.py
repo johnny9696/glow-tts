3050e5fc75ce20b3791244adbf24ccf7fff6fca5
johnny <johnny960423@gmail.com>
2023-03-17 14:47:01 +0900

    GE2E_data_loader updates

--- Speaker_Encoder/mel_data.py
+++ Speaker_Encoder/mel_data.py
@@ -168,3 +168,76 @@ class MelSIDCollate():
             
 
         return (mel_padded, sid)
+
+
+class Mel_GE2E(torch.utils.data.Dataset):
+    def __init__(self, audio_path, hps):
+        self.audio_path = audio_path
+        self.sid_list = os.listdir(self.audio_path)
+        if hps.train.utterance != len(sid_list):
+            sys.exit('Utterance({}) and Speaker number({}) is not correct'.format(len(sid_list), hps.train.utterance))
+        self.speaker_per_wav = []
+        for i in self.sid_list:
+            tmp = os.path.join(self.audio_path, i)
+            wav_list = os.listdir(tmp)
+            self.speaker_per_wav.append(len(wav_list))
+        self.speaker_min_wav = [x for x in range(min(self.speaker_per_wav))]
+        self.max_wav_value = hps.data.max_wav_value
+        self.sampling_rate = hps.data.sampling_rate
+        self.filter_length = hps.data.filter_length
+        self.hop_length = hps.data.hop_length
+        self.win_length = hps.data.win_length
+        self.n_mels = hps.data.n_mel_channnels
+        self.mel_fmin = hps.data.mel_fmin
+        self.mel_fmax = hps.data.mel_fmax
+        self.add_noise = hps.data.add_noise
+        self.slice_length = ((self.sampling_rate*hps.data.slice_length)//self.filter_length) * (self.filter_length//self.hop_length)
+        self.utterance = hps.train.utterance
+        self.stft = commons.TacotronSTFT(
+            self.filter_length, self.hop_length, self.win_length,
+            self.n_mel_channels, self.sampling_rate, self.mel_fmin,
+            self.mel_fmax)
+
+        random.seed(1234)
+        random.shuffle(self.speaker_min_wav)
+
+    def get_mel(self, filename):
+        if not self.load_mel_from_disk:
+            audio, sampling_rate = load_wav_to_torch(filename)
+            if sampling_rate != self.stft.sampling_rate:
+                raise ValueError("{} {} SR doesn't match target {} SR".format(
+                    sampling_rate, self.stft.sampling_rate))
+            if self.add_noise:
+                audio = audio + torch.rand_like(audio)
+            audio_norm = audio / self.max_wav_value
+            audio_norm = audio_norm.unsqueeze(0)
+            melspec = self.stft.mel_spectrogram(audio_norm)
+            melspec = torch.squeeze(torch.tensor(melspec), 0)
+        else:
+            melspec = torch.from_numpy(np.load(filename))
+            assert melspec.size(0) == self.stft.n_mel_channels, (
+                'Mel dimension mismatch: given {}, expected {}'.format(
+                    melspec.size(0), self.stft.n_mel_channels))
+        melspec=torch.abs(melspec)/torch.max(torch.abs(melspec))
+        return melspec
+
+
+    def __getitem__(self, index):
+        audio_path_tmp= self.sid_list[index]
+        wav_path = os.listdir(os.path.join(self.audio_path,audio_path_tmp))
+        random.shuffle(wav_path)
+        wav_path = wav_path[:self.utterance]
+        mel_padded = torch.FloatTensor(self.utterance, self.n_mels, self.slice_length)
+        mel_padded.zero_()
+        for i, path in enumerate(wav_path):
+            mel_db = self.get_mel(path)
+            mel_db=torch.abs(mel_db)/torch.max(torch.abs(mel_db))
+            if mel_db.size(1) > self.frames :
+                mel_padded[i, :, :self.frames] = mel_db[:, :self.frames]
+            else : 
+                mel_padded[i, :, :mel_db.size(1)] = mel_db
+        return mel_padded
+
+
+    def __len__(self):
+        return len(self.sid_list)
